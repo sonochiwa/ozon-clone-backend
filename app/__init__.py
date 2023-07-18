72fc3deebbb2cecbe1659bed979ca62da0369f32
Rashid Mamarimov <sonochiwaxd@gmail.com>
2023-07-19 00:57:57 +0600

    chore: move cors to middleware

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from sqladmin import Admin, ModelView
 
 from app.user.model import User
 from core.config import config
-from core.db.session import engine
+from core.fastapi.middlewares.cors import init_cors
 from core.imports.import_routes import routes
 
 
@@ -13,31 +12,18 @@ def init_routers(_app: FastAPI) -> None:
 
 def create_app():
     _app = FastAPI(
-        title='Ozon сlone API docs',
+        title='Ozon clone API docs',
         docs_url=None if config.ENV == 'production' else '/docs',
     )
-    _admin = Admin(_app, engine)
-
-    class UserAdmin(ModelView, model=User):
-        column_list = [User.id, User.first_name]
-
-    _admin.add_view(UserAdmin)
-
+    init_cors(_app)
     init_routers(_app)
 
-    from starlette.middleware.cors import CORSMiddleware
-
-    origins = [
-        'http://localhost:3000'
-    ]
-
-    _app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-        allow_headers=['Content-Type', 'Set-Cookie', 'Access-Control-Allow-Headers', 'X-Total-Count'],
-    )
+    # admin = Admin(_app, engine)
+    #
+    # class UserAdmin(ModelView, model=User):
+    #     column_list = [User.id, User.first_name]
+    #
+    # admin.add_view(UserAdmin)
 
     return _app
 
@@ -47,7 +33,7 @@ app = create_app()
 
 @app.on_event("startup")
 async def startup_event():
-    print('INFO:     docs - http://0.0.0.0:8000/docs')
+    print(f'INFO:     docs - http://{config.APP_HOST}:{config.APP_PORT}/docs')
 
 
 @app.on_event("shutdown")
--- core/fastapi/middlewares/cors.py
+++ core/fastapi/middlewares/cors.py
@@ -1,15 +1,16 @@
+from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from app import app
-
 origins = [
-    'http://localhost:3000/'
+    'http://localhost:3000',
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allow_headers=['*'],
-)
+
+def init_cors(_app: FastAPI) -> None:
+    return _app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        allow_headers=['Content-Type', 'Set-Cookie', 'Access-Control-Allow-Headers', 'X-Total-Count'],
+    )
